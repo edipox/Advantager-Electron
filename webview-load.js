11041a247414bd65c364c58a9cbad41f4906f7d5
Edipo Vinicius da Silva <edipox@gmail.com>
2016-09-17 22:49:37 -0400

    Fix background issue

--- webview-load.js
+++ webview-load.js
@@ -1,7 +1,14 @@
 window.onload = function() {
   var onReady = function(){
-    $("#top-menu, #header, #wrapper").attr("style", "background: none!important; -webkit-app-region: drag!important;")
-    $("a, input, textarea, button").attr("style", "-webkit-app-region: no-drag!important;")
+    var appendStyle  = function(element, style){
+      element.attr("style", style+";"+element.attr("style"));
+    }
+    var $header = $("#top-menu, #header, #wrapper");
+    appendStyle($header, "background: none!important; -webkit-app-region: drag!important");
+    var $notDraggable = $("a, input, textarea, button");
+    appendStyle($notDraggable, "-webkit-app-region: no-drag!important");
+    var containers = "html, body, #wrapper, #wrapper1, #wrapper2, #wrapper3, #main, #content".split(", ");
+    $.each(containers, function(index, cont){ appendStyle($(cont), "height: 100%"); })
   }
   $(document).ready(onReady).on("turbolinks:load", onReady);
 };
